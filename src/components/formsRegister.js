9fbd9e94e63d98b12784bfa63daaf12fac8f52d1
ClsRazo <carlosrazo2801@hotmail.com>
2024-11-25 22:09:10 -0600

    Validacion de fortaleza de contraseñas

--- src/components/formsRegister.js
+++ src/components/formsRegister.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+//Biblioteca para la validacion de contraseñas, aparte de usar regex, será zxcvbn
+import zxcvbn from "zxcvbn";
 
 const Register = () => {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
 
     const aBufferToHex = (buffer) => {
         return [...new Uint8Array(buffer)].map((b) => b.toString(16).padStart(2,"0")).join("");
@@ -11,6 +14,21 @@ const Register = () => {
 
     const handleRegister = async (e) => {
         e.preventDefault();
+
+        //Checamos que las dos contraseñas sean iguales
+        if(password !== passwordConfirm){
+            alert("Las contraseñas no coinciden");
+            return;
+        }
+
+        //Checamos la fortaleza de la contraseña
+        const resultado = zxcvbn(password);
+        console.log("Fortaleza de la contraseña: ", resultado.score);
+
+        if(resultado.score < 3){
+            alert("La contraseña es muy débil");
+            return;
+        }
 
         const encoder = new TextEncoder(); //Para convertir la cadena a un arreglo de bytes
         const datos = encoder.encode(password); //.encode() convierte la cadena a un arreglo de bytes
@@ -80,6 +98,8 @@ const Register = () => {
                     <label>Confirmar contraseña:</label>
                     <input
                         type="password"
+                        value={passwordConfirm}
+                        onChange={(e) => setPasswordConfirm(e.target.value)}
                         required
                     />
                 </div>
